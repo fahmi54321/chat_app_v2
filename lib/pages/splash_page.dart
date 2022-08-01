98d14fbbbc6572d3470321a5ad9c587888908bcc
fahmi54321 <fahmiabdulaziz44@gmail.com>
2022-08-01 20:37:25 +0700

    splash page and internal service

---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,56 @@
+import 'package:chat_app/services/cloud_storage_services.dart';
+import 'package:chat_app/services/database_services.dart';
+import 'package:chat_app/services/media_services.dart';
+import 'package:chat_app/services/navigation_services.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:get_it/get_it.dart';
+
+class SplashPage extends StatefulWidget {
+  final VoidCallback onInitializationComplete;
+
+  const SplashPage({
+    Key? key,
+    required this.onInitializationComplete,
+  }) : super(key: key);
+
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 1)).then((_) {
+      _setup().then((_) => widget.onInitializationComplete());
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Container(
+        height: 200,
+        width: 200,
+        child: const Icon(Icons.flutter_dash),
+      ),),
+    );
+  }
+
+  //todo 1 (inisial firebase)
+  Future<void> _setup() async{
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+    _registerServices();
+  }
+
+  //todo 2 register service yang dibutuhkan (next main)
+  void _registerServices(){
+    GetIt.instance.registerSingleton<NavigatorServices>(NavigatorServices());
+    GetIt.instance.registerSingleton<MediaService>(MediaService());
+    GetIt.instance.registerSingleton<CloudStorageServices>(CloudStorageServices());
+    GetIt.instance.registerSingleton<DatabaseServices>(DatabaseServices());
+  }
+}
---
+++ lib/services/cloud_storage_services.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
+
+const String USER_COLLECTION= 'Users';
+
+class CloudStorageServices{
+
+  final FirebaseStorage _storage = FirebaseStorage.instance;
+
+  CloudStorageServices(){}
+
+}
