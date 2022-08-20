99cb089b48f1a9ae62e90fb8b9c55d272bb618d6
fahmi54321 <fahmiabdulaziz44@gmail.com>
2022-08-20 10:55:21 +0700

    layouting login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,12 +1,113 @@
+import 'package:chat_app/widgets/custom_input_fields.dart';
+import 'package:chat_app/widgets/rounded_button.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+//todo 1 (finish)
+
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  late double _deviceHeight;
+  late double _deviceWidth;
+
+  final _loginFormKey = GlobalKey<FormState>();
+
+  @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      appBar: AppBar(title: Text('test'),),
+      body: Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: _deviceWidth * 0.03,
+          vertical: _deviceHeight * 0.02,
+        ),
+        height: _deviceHeight * 0.98,
+        width: _deviceWidth * 0.97,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _pageTitle(),
+            SizedBox(height: _deviceHeight * 0.04),
+            _loginForm(),
+            SizedBox(height: _deviceHeight * 0.05),
+            _loginButton(),
+            SizedBox(height: _deviceHeight * 0.02),
+            _registerAccountLink(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _pageTitle() {
+    return SizedBox(
+      height: _deviceHeight * 0.10,
+      child: const Text(
+        'Chat',
+        style: TextStyle(
+          fontSize: 40,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+    );
+  }
+
+  Widget _loginForm() {
+    return SizedBox(
+      height: _deviceHeight * 0.18,
+      child: Form(
+        key: _loginFormKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            CustomTextFormField(
+              onSaved: (_value) {},
+              regEx:
+                  r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$",
+              hintText: 'Email',
+              obscureText: false,
+            ),
+            CustomTextFormField(
+              onSaved: (_value) {},
+              regEx: r".{8,}",
+              hintText: 'Password',
+              obscureText: true,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _loginButton() {
+    return RoundedButton(
+      name: 'Login',
+      height: _deviceHeight * 0.065,
+      width: _deviceHeight * 0.65,
+      onPressed: () {},
+    );
+  }
+
+  Widget _registerAccountLink() {
+    return GestureDetector(
+      onTap: (){},
+      child: const Text(
+        'Dont have a account?',
+        style: TextStyle(
+          color: Colors.blueAccent,
+        ),
+      ),
     );
   }
 }
