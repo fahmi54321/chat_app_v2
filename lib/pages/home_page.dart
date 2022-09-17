39236df472a900005ebe03b9731c6de013ec5e88
fahmi54321 <fahmiabdulaziz44@gmail.com>
2022-09-17 16:49:47 +0700

    layouting home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,49 @@
+import 'package:chat_app/pages/chats_page.dart';
+import 'package:chat_app/pages/users_page.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+//todo 1 (finish)
+
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentPage = 0;
+  final List<Widget> _pages = [
+    ChatsPage(),
+    UsersPage(),
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: _pages[_currentPage],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentPage,
+        onTap: (index) {
+          setState(() {
+            _currentPage = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            label: 'Chats',
+            icon: Icon(
+              Icons.chat_bubble,
+            ),
+          ),
+          BottomNavigationBarItem(
+            label: 'Users',
+            icon: Icon(
+              Icons.supervised_user_circle_sharp,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
